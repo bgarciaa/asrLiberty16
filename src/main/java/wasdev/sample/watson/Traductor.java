eab36efc814d53cb4017d5da96a57875c500aef2
Bea <201207044@alu.comillas.edu>
2017-04-06 21:06:49 +0200

    traductor

---
+++ src/main/java/wasdev/sample/watson/Traductor.java
@@ -0,0 +1,30 @@
+package wasdev.sample.watson;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.Language;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
+
+public class Traductor
+{
+	public static String translate(String palabra)
+	{
+		LanguageTranslator service = new LanguageTranslator();
+		service.setUsernameAndPassword("USER", "PASSWORD");
+		service.setEndPoint("https://gateway.watsonplatform.net/languagetranslator/api");
+		TranslationResult translationResult = service.translate(palabra, Language.SPANISH, Language.ENGLISH).execute();
+		String traduccionJSON = translationResult.toString();
+		System.out.println(traduccionJSON+"\n-------\n");
+		JsonParser parser = new JsonParser();
+		JsonObject rootObj = parser.parse(traduccionJSON).getAsJsonObject();
+		String wordCount = rootObj.get("word_count").getAsString();
+		JsonArray traducciones = rootObj.getAsJsonArray("translations");
+		String traduccionPrimera = palabra;
+		if(traducciones.size()>0)
+			traduccionPrimera = traducciones.get(0).getAsJsonObject().get("translation").getAsString();
+		return traduccionPrimera;
+		
+	}
+}
